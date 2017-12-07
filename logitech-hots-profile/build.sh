aac1a5f5e0f874a5a542ae3cf14a1918c46f48c0
Viktor (Icon) VAD <vad.viktor@gmail.com>
2017-12-07 10:20:39 +0000

    Adding extra build params to remove debug information.

--- logitech-hots-profile/build.sh
+++ logitech-hots-profile/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a -o /media/sf_vmshared/logitech-hots-profile.exe
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -a -o /media/sf_vmshared/logitech-hots-profile.exe
